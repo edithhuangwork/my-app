331e9cdafe61ad1bc2cc0f40fce4542a58f17a5f
edithhuangwork <edith@EdithdeMacBook-Air.local>
2026-03-10 11:46:24 +0800

    react deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/FE-React/",
+  build: {
+    outDir: 'dist',
+    assetDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chuckFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
